20bed4a182da48377d05b2df0da76001efc106dc
Lee Sun Hong <nuseehr@naver.com>
2021-05-02 15:58:33 +0900

    Add Leet1 Problem

--- 6-String-Manipulation/Leet1_TwoSum.py
+++ 6-String-Manipulation/Leet1_TwoSum.py
@@ -11,10 +11,20 @@
     
 '''
 
-# Brute Force 를 이용한 풀이.
+# 1. Brute Force 를 이용한 풀이.
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         for idx1, val1 in enumerate(nums):
             for idx2, val2 in enumerate(nums):
                 if idx1 != idx2 and val1+val2 == target:
                     return [idx1, idx2]
+                    
+                    
+# 2. Brute Force, 속도개선.
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        for idx, val in enumerate(nums):
+            ans = target - val
+            
+            if ans in nums[idx + 1:]:
+                return[idx, nums[idx + 1:].index(ans) + (idx + 1)]
